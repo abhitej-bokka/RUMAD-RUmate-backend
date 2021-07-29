58f1a6067207bef2ebd7be7bdc1f042e7d6d24e3
Aryan Patel <adp2582@gmail.com>
2021-07-28 22:16:57 -0400

    added db connection

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(
+
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log('connected to database!')
+
+);
+
 app.listen(3000, () => console.log('Server is now running!'));
